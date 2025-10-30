c5195066190f0925e53850993144c85fa766cc9d
dafina.kastrati <dafina.kastrati@uni-konstanz.de>
2025-10-30 15:25:46 +0100

    small change

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 
 app.use(express.json());
 
-// Beispiel-Route
+// Beispiel-GET-Route
 app.get('/api/scooters', (req, res) => {
   const scooters = [
     { id: 1, name: 'Scooter A' },
-    { id: 2, name: 'Scooter B' },
+    { id: 2, name: 'Scooter C' },
   ];
   res.json(scooters);
 });
 
-// Beispiel-POST-Route
+// Beispiel-POST-Route 
 app.post('/api/scooters', (req, res) => {
   const newScooter = req.body;
   newScooter.id = 3; // Fake-ID
