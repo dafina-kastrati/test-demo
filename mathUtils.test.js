876aa0fff7b15f23553f26523d7f07a61d312e52
dafina.kastrati <dafina.kastrati@uni-konstanz.de>
2025-10-30 15:02:13 +0100

    Initial commit - Express API with tests

---
+++ mathUtils.test.js
@@ -0,0 +1,9 @@
+const { add, multiply } = require('./mathUtils');
+
+test('adds two numbers correctly', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('multiplies two numbers correctly', () => {
+  expect(multiply(4, 5)).toBe(20);
+});
